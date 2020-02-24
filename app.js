d3909c74db6180b2e719ed5363dfa4598ec941b5
Qixiang <qixiang100@gmail.com>
2020-02-24 00:11:41 -0500

    Finished building twitter clone with mern stack

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "jim",
+    email: "jim@jim.jim",
+    password: "jimisgreat123"
+  })
+  user.save()
+  res.send("Hello World!");
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+
